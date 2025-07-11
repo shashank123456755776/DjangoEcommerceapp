55a931e661a585142e308eb715674bbaa449c954
shashank123456755776 <114920880+shashank123456755776@users.noreply.github.com>
2025-07-11 12:41:58 +0530

    commit2

--- ecommerce/ecommerceapp/models.py
+++ ecommerce/ecommerceapp/models.py
@@ -7,7 +7,20 @@ class Contact(models.Model):
     desc=models.TextField(null=True, blank=True,max_length=500)
     phonenumber=models.IntegerField()
     def __str__(self):
-        return super().__str__()
+        return self.name
+class Product(models.Model):
+    product_id = models.AutoField
+    product_name =  models.CharField(max_length=100)
+    category=models.CharField(max_length=100,default="")
+    sub_category=models.CharField(max_length=50,default="")
+    price= models.IntegerField(default=0)
+    desc=models.CharField(max_length=300)
+    image= models.ImageField(upload_to='images/images',default="")
+    
+    
+    def __str__(self):
+        return self.product_name
+    
     
     
     
--- ecommerce/ecommerceapp/views.py
+++ ecommerce/ecommerceapp/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
-from ecommerceapp.models import Contact
+from ecommerceapp.models import Contact,Product
 from django.contrib import messages
+from math import ceil
 
 
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+    allProds =[]
+    catprods= Product.objects.values('category','id')
+    # print(catprods)
+    cats ={item['category'] for item in catprods}
+    # print(cats)
+    for cat in cats:
+        prod=Product.objects.filter(category=cat)
+        print(prod)
+        n=len(prod)
+        nSlides=n//4+ ceil((n/4)-(n//4))
+        allProds.append([prod,range(1,nSlides),nSlides])
+    params ={'allProds':allProds}    
+    return render(request,"index.html",params)
+    
 def contact(request):
     if request.method =="POST":
         name=request.POST.get("name")
